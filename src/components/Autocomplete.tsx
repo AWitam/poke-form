eefa7aa4168e6826e0d5cd0fe1d49cc31148e914
Ada <witamborska.ada@gmail.com>
2024-08-11 17:35:34 +0200

    feat: configure theme; override default component styling; add font

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import React, { ElementType } from "react";
+import { alpha, ChipTypeMap, Paper, Popper, styled, TextField, TextFieldProps } from "@mui/material";
+import MUIAutocomplete, { AutocompleteProps as MUIAutocompleteProps } from "@mui/material/Autocomplete";
+
+const StyledAutocomplete = styled(MUIAutocomplete)(({ theme }) => ({
+  "& .MuiAutocomplete-inputRoot": {
+    margin: "0.125rem 0",
+    padding: " 0.875rem 0.625rem",
+    "&:hover fieldset": {
+      borderColor: theme.palette.primary.main,
+    },
+
+    "& .MuiAutocomplete-input": {
+      color: theme.palette.grey[100],
+      padding: 0,
+      lineHeight: "1.25rem",
+      fontSize: "0.875rem",
+      fontWeight: 400,
+      height: "auto",
+
+      "&::placeholder": {
+        color: theme.palette.grey[200],
+      },
+    },
+  },
+
+  "& .Mui-focused": {
+    "& .MuiOutlinedInput-notchedOutline": {
+      boxShadow: `0 0 0 4px ${theme.palette.action.focus}`,
+    },
+  },
+
+  "& .MuiFormHelperText-root": {
+    margin: "0",
+    color: theme.palette.grey[100],
+  },
+})) as typeof MUIAutocomplete;
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  "& .MuiAutocomplete-listbox": {
+    color: theme.palette.common.black,
+
+    "& .MuiAutocomplete-option": {
+      padding: "0.25rem 0.75rem",
+      lineHeight: "1.25rem",
+      fontSize: "0.875rem",
+    },
+    "& .MuiAutocomplete-option[aria-selected='true']": {
+      backgroundColor: "transparent",
+      color: theme.palette.primary.main,
+
+      "&.Mui-focused": {
+        backgroundColor: theme.palette.action.focus,
+      },
+    },
+  },
+  "& .MuiAutocomplete-listbox .MuiAutocomplete-option.Mui-focused": {
+    backgroundColor: theme.palette.action.focus,
+  },
+
+  boxShadow: `0 4px 10px ${alpha(theme.palette.common.black, 0.1)}`,
+}));
+
+type DefaultChip = ChipTypeMap["defaultComponent"];
+
+interface AutocompleteProps<
+  Value,
+  Multiple extends boolean = false,
+  DisableClearable extends boolean = false,
+  FreeSolo extends boolean = false,
+  ChipComponent extends ElementType = DefaultChip
+> extends Omit<MUIAutocompleteProps<Value, Multiple, DisableClearable, FreeSolo, ChipComponent>, "renderInput"> {
+  inputProps: TextFieldProps;
+}
+
+export const Autocomplete = <
+  Value,
+  Multiple extends boolean,
+  DisableClearable extends boolean,
+  FreeSolo extends boolean,
+  ChipComponent extends ElementType = DefaultChip
+>({
+  inputProps,
+  ...props
+}: AutocompleteProps<Value, Multiple, DisableClearable, FreeSolo, ChipComponent>) => {
+  return (
+    <StyledAutocomplete
+      fullWidth
+      renderInput={(props) => <TextField {...props} {...inputProps} />}
+      PaperComponent={(props) => <StyledPaper {...props} />}
+      {...props}
+    />
+  );
+};
--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { createTheme } from "@mui/material/styles";
+import { createTheme, PaletteOptions } from "@mui/material/styles";
 import localFont from "next/font/local";
 
 const IBM_VGA = localFont({
@@ -12,5 +12,50 @@ const IBM_VGA = localFont({
 export const theme = createTheme({
   typography: {
     fontFamily: IBM_VGA.style.fontFamily,
+  },
+  palette: {
+    primary: {
+      main: "rgba(151, 71, 255, 1)",
+      dark: "rgba(113, 53, 191, 1)",
+      light: "rgba(151, 71, 255, 0.25)",
+    },
+    grey: {
+      "100": "rgba(42, 42, 42, 1)",
+      "200": "rgba(127, 127, 127, 1)",
+      "300": "rgba(228, 228, 228, 1)",
+      "400": "rgba(238, 238, 238, 1) ",
+    },
+    error: {
+      main: "rgba(255, 78, 78, 1) ",
+    },
+    background: {
+      default: "rgba(255, 255, 255, 1)",
+    },
+    action: {
+      focus: "rgba(151, 71, 255, 0.25)",
+      disabledBackground: "rgba(0, 0, 0, 0.2)",
+    },
+  },
+  shape: {
+    borderRadius: 2,
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        hiddenLabel: true,
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme: { palette } }) => ({
+          color: palette.common.black,
+        }),
+      },
+    },
   },
 });
